62d5381cd3d9bc8aecc4e2d67d7319710dd58975
Bryan Souza Quadrado <bryanquadrado2@gmail.com>
2023-01-21 22:14:06 -0300

    External Dependencies

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"module/auxiliar"
 	"fmt"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+	erro := checkmail.ValidateFormat("bryan@gmail.com")
+	fmt.Println(erro)
 
 }
