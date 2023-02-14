b1ae56904b922b74d25628341351c2357cacf18e
Bryan Quadrado <bryanquadrado2@gmail.com>
2023-02-14 11:49:25 -0300

    app command line

---
+++ 17 - Commandline App/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"linha-de-comando/app"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("Start Point")
+
+	aplicacao := app.Gerar()
+	if erro := aplicacao.Run(os.Args); erro != nil{
+		log.Fatal(erro)
+	}
+	
+}
